71c8bcf062998f6ecc0d3cf46acaa0a4b450cc57
Dave Gillard <dave.gillard@ed.ac.uk>
2021-10-31 13:21:43 +0000

    Move to module

--- root.tf
+++ root.tf
@@ -55,59 +55,15 @@ resource "aws_security_group" "web" {
     }
 }
 
-resource "aws_elb" "web" {
-    name    = "web"
+module "prod" {
+    source = "./modules/prod"
 
-    subnets = [
-        aws_default_subnet.default_az1.id,
-        aws_default_subnet.default_az2.id,
-    ]
-
-    security_groups = [
-        aws_security_group.web.id
-    ]
-
-    listener {
-        instance_port     = 80
-        instance_protocol = "http"
-        lb_port           = 80
-        lb_protocol       = "http"
-    }
-
-    tags =  {
-        "Terraform" = "true"
-    }
-}
-
-resource "aws_launch_template" "web" {
-    name_prefix   = "web"
-    image_id      = var.web_image_id
-    instance_type = var.web_instance_type
-}
-
-resource "aws_autoscaling_group" "web" {
-    vpc_zone_identifier = [
-        aws_default_subnet.default_az1.id,
-        aws_default_subnet.default_az2.id,
-    ]
-
-    desired_capacity   = var.web_desired_capacity
-    max_size           = var.web_max_size
-    min_size           = var.web_min_size
-
-    launch_template {
-        id      = aws_launch_template.web.id
-        version = "$Latest"
-    }
-
-    tag {
-       key                 = "Terraform" 
-       value               = "true"
-       propagate_at_launch = true
-    }
-}
-
-resource "aws_autoscaling_attachment" "asg_attachment_bar" {
-    autoscaling_group_name = aws_autoscaling_group.web.id
-    elb                    = aws_elb.web.id
+    web_app              = "prod"
+    web_image_id         = var.web_image_id
+    web_instance_type    = var.web_instance_type
+    web_desired_capacity = var.web_desired_capacity
+    web_max_size         = var.web_max_size
+    web_min_size         = var.web_min_size
+    subnets              = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+    security_groups      = [aws_security_group.web.id]
 }
